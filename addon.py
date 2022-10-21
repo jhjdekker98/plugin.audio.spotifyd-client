b4cdfec27cfa061c36db5fb6fd1e6f58a023985a
Jeroen Dekker <j.hj.dekker.98@gmail.com>
2022-10-21 19:45:39 +0200

    Move Connect to service, add GUI + necessary configurable settings

--- addon.py
+++ addon.py
@@ -1,9 +1,97 @@
-import xbmcaddon, xbmcgui
-import subprocess
+import xbmc, xbmcaddon, xbmcgui
+import service
+import os
+import requests, base64
+import threading
+import time
 
-addon = xbmcaddon.Addon()
-addonName = addon.getAddonInfo('name')
-spotifyPath = addon.getAddonInfo('path') + 'resources/lib/spotifyd/spotifyd'
+SPOTIFYD_LOG_READ_LINE_AMOUNT = 100
+SPOTIFY_API_URL_TOKEN = 'https://accounts.spotify.com/api/token'
+SPOTIFY_API_URL_TRACK = 'https://api.spotify.com/v1/tracks/'
 
-xbmcgui.Dialog().ok(addonName, spotifyPath)
-subprocess.run(spotifyPath)
+currSong = '<song>'
+currArtist = '<artist>'
+accessToken = ''
+accessTokenExpire = 0
+
+class GUI(xbmcgui.WindowXML):
+    def __init__(self, *args, **kwargs):
+        self.data = kwargs['optional1']
+
+    def onInit(self):
+        xbmc.executebuiltin('Container.SetViewMode(50)')
+
+def writeResponse(response):
+    f = open("/tmp/responses.log", "a")
+    f.write(str(response) + "\n")
+    f.write(str(response.headers) + "\n")
+    f.write(str(response.content) + "\n\n")
+    f.close()
+
+def updateTrackData(window):
+    accessToken = ''
+    accessTokenExpire = 0
+    lastTrackId = ''
+
+    while True:
+        trackData = getTrackData(lastTrackId, accessToken, accessTokenExpire)
+        if trackData != None:
+            accessToken = trackData["accessToken"]
+            accessTokenExpire = trackData["accessTokenExpire"]
+            lastTrackId = trackData["lastTrackId"]
+            window.setProperty("currSong", trackData["json"]["name"])
+            window.setProperty("currArtist", trackData["json"]["artists"][0]["name"])
+            images = trackData["json"]["album"]["images"]
+            window.setProperty("albumArt", images[len(images)-1]["url"])
+
+def getTrackData(lastTrackId, accessToken, accessTokenExpire):
+    trackId = getTrackId()
+    if(trackId == lastTrackId):
+        return None
+
+    currTime = int(time.time())
+
+    if currTime >= accessTokenExpire:
+        addon = xbmcaddon.Addon('plugin.audio.spotifyd-client')
+        clientId = addon.getSetting('clientId')
+        clientSecret = addon.getSetting('clientSecret')
+
+        b64 = base64.b64encode(f'{clientId}:{clientSecret}'.encode('ascii')).decode('ascii')
+        headers = {'Authorization': f'Basic {b64}'}
+        body = {'grant_type': 'client_credentials'}
+        response = requests.post(SPOTIFY_API_URL_TOKEN, headers=headers, data=body)
+        writeResponse(response)
+        accessToken = response.json()['access_token']
+        accessTokenExpire = currTime + response.json()['expires_in']
+
+    headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + accessToken
+    }
+    response = requests.get(SPOTIFY_API_URL_TRACK + trackId, headers=headers)
+    writeResponse(response)
+    return {
+        'accessToken': accessToken,
+        'accessTokenExpire': accessTokenExpire,
+        'lastTrackId': trackId,
+        'json': response.json()
+    }
+
+def getTrackId():
+    addonPath = service.getAddonPath()
+    output = os.popen(f'tail {addonPath}songs.log -n {SPOTIFYD_LOG_READ_LINE_AMOUNT} | grep Loading | tail -1').read()
+    x = output.find('<spotify:track:')
+    if (x == -1):
+        return None
+    return output[x+15:-2]
+
+if (__name__ == '__main__'):
+    xbmc.executebuiltin('Dialog.Close(busydialog)')
+    xbmc.executebuiltin('InhibitScreensaver(true)')
+    ui = GUI('w_main.xml', service.getAddonPath(), 'default', '1080i', True, optional1='data')
+    update_thread = threading.Thread(target=updateTrackData, args=(ui,))
+    update_thread.daemon = True
+    update_thread.start()
+    ui.doModal()
+    del ui
---
+++ service.py
@@ -0,0 +1,27 @@
+import xbmcaddon
+import subprocess, os
+
+addon = xbmcaddon.Addon()
+addonName = addon.getAddonInfo('name')
+addonPath = addon.getAddonInfo('path')
+spotifyPath = addonPath + 'resources/lib/spotifyd/spotifyd'
+
+def getAddonPath():
+    return addonPath
+
+def startUp():
+    f = open(f"{addonPath}songs.log", "w")
+    f.truncate()
+    subprocess.Popen([spotifyPath, '--no-daemon'], stdout=f)
+
+def shutDown():
+    subprocess.run(["kill", "$(ps | grep spotify | awk '{print $1}')"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True)
+
+def restart():
+    shutDown()
+    startUp()
+
+#Check if already running
+output = os.popen("ps | grep spotify | grep -v grep").read()
+if output.count("\n") == 0:
+    startUp()
